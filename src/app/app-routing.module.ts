73bcb3b72f7a5cee70d86a1540561e7183617001
Carlyle Komalram <glaw14@gmail.com>
2022-08-10 21:00:25 -0500

    agregando rutas secundarias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MainPageComponent } from './pages/main-page.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'grafica1', component: Grafica1Component},
-  { path: 'progress', component: ProgressComponent},
-  { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent},
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  { path: '**', component: NotFoundComponent}
+  {
+    path: '',
+    component: MainPageComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: 'progress', component: ProgressComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
-
+import { MainPageComponent } from './pages/main-page.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +25,8 @@ import { Grafica1Component } from './pages/grafica1/grafica1.component';
     SidebarComponent,
     HeaderComponent,
     ProgressComponent,
-    Grafica1Component
+    Grafica1Component,
+    MainPageComponent
   ],
   imports: [
     BrowserModule, AppRoutingModule
