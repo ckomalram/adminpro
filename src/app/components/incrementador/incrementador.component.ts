129ddaa7705580c4d292ca10588f4283b2a1e81f
Carlyle Komalram <glaw14@gmail.com>
2022-08-11 18:14:46 -0500

    puliendo detalles -incrementador

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class IncrementadorComponent   {
 
  @Input() progreso: number = 10;
+ @Input() btnclass: string = 'btn btn-primary';
  @Output() valorBarra: EventEmitter<number> = new EventEmitter();
 //  @Input('Renombrado') progreso: number = 10;
 
@@ -24,6 +25,18 @@ export class IncrementadorComponent   {
       return this.progreso = 0;
     }
     this.progreso += valor;
+    this.valorBarra.emit(this.progreso);
+  }
+
+  onChange(event: number){
+    // console.log(event);
+    if (event >= 100) {
+      this.progreso=100;
+    }else if (event < 0){
+      this.progreso=0;
+    }else{
+      this.progreso=event;
+    }
     this.valorBarra.emit(this.progreso);
   }
 
