59384b92ee962062999952efbd08dacedf0b5b93
Carlyle Komalram <glaw14@gmail.com>
2022-08-11 13:30:26 -0500

    agregando progress bar-css-html

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
-  templateUrl: './progress.component.html'
+  templateUrl: './progress.component.html',
+  styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent implements OnInit {
 
