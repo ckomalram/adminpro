20e28051f01a01df63eb7bee9cb5ff0f27ab234c
Carlyle Komalram <glaw14@gmail.com>
2022-08-11 13:44:54 -0500

    agregando logica - progress

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+  progreso: number = 14;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get getPorcentaje(){
+    return `${this.progreso}%`;
   }
+
+  cambiarValor(valor: number): any{
+    if (this.progreso >= 100 && this.progreso >= 0) {
+      return this.progreso = 100;
+    }
+    if (this.progreso <= 0 && this.progreso < 0) {
+      return this.progreso = 0;
+    }
+    this.progreso += valor;
+  }
+
 
 }
