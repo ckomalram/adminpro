bcc20c914a108c276e3f6e5e0d47fbd95dfb2f84
Carlyle Komalram <glaw14@gmail.com>
2022-08-17 23:37:37 -0500

    agregando logica para check css de settings

--- src/app/pages/account-setting/account-setting.component.ts
+++ src/app/pages/account-setting/account-setting.component.ts
@@ -7,20 +7,34 @@ import { Component, OnInit } from '@angular/core';
 export class AccountSettingComponent implements OnInit {
 
   public  elemento = document.querySelector('#theme');
+  public links!: NodeListOf<Element>;
   constructor() { }
 
   ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
   }
 
   changeTheme(theme: string){
-    // console.log(theme);
     const url = `./assets/css/colors/${theme}.css`;
     this.elemento?.setAttribute('href', url);
     localStorage.setItem('theme', url);
-    // console.log(url);
-    // console.log(elemento);
+    this.checkCurrentTheme();
 
+  }
 
+  checkCurrentTheme(){
+
+    this.links.forEach(elemento => {
+      elemento.classList.remove('working');
+      const btnTheme = elemento.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.elemento?.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        elemento.classList.add('working');
+      }
+    });
   }
 
 }
