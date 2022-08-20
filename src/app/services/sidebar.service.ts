2a4a1987a757d0d36a21751601352d3f9c7ed62c
Carlyle Komalram <glaw14@gmail.com>
2022-08-20 14:56:03 -0500

    agregando servicio para menu dinamico

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [
+    {
+      titulo: 'ADV',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        {titulo: 'Main' , url: '/'},
+        {titulo: 'ProgressBar' , url: 'progress'},
+        {titulo: 'Gráficas' , url: 'grafica1'}
+      ]
+    },
+    {
+      titulo: 'ADV Comercial',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        {titulo: 'Main' , url: '/'},
+        {titulo: 'ProgressBar' , url: 'progress'},
+        {titulo: 'Gráficas' , url: 'grafica1'}
+      ]
+    }
+    ,
+    {
+      titulo: 'Ejecutivo Ventas',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        {titulo: 'Main' , url: '/'},
+        {titulo: 'ProgressBar' , url: 'progress'},
+        {titulo: 'Gráficas' , url: 'grafica1'}
+      ]
+    }
+  ];
+
+  constructor() { }
+}
