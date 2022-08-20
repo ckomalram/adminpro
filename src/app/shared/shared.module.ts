35f1812783429c83d16fae2061d5e729e72e0312
Carlyle Komalram <glaw14@gmail.com>
2022-08-20 14:37:12 -0500

    agregando routerLink de ruta settings

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BradcrumbsComponent } from './bradcrumbs/bradcrumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [BradcrumbsComponent, HeaderComponent, SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   exports: [BradcrumbsComponent, HeaderComponent, SidebarComponent],
 })
