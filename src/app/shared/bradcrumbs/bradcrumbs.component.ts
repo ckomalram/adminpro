a5d8c9923efb2768ee0a1cd772efd250ed4a7b7e
Carlyle Komalram <glaw14@gmail.com>
2022-08-20 23:59:55 -0500

    rxjs - agregando unsubcribe a breadcumbs

--- src/app/shared/bradcrumbs/bradcrumbs.component.ts
+++ src/app/shared/bradcrumbs/bradcrumbs.component.ts
@@ -1,35 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
-import { filter, map } from 'rxjs';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bradcrumbs',
   templateUrl: './bradcrumbs.component.html'
 })
-export class BradcrumbsComponent implements OnInit {
+export class BradcrumbsComponent implements OnInit, OnDestroy {
 
   titulo: string = '';
+  tituloSub$: Subscription;
 
   constructor(private router: Router) {
-    this.getDataRuta();
-   }
+    this.tituloSub$ = this.getDataRuta()
+      .subscribe((data: any) => {
+        console.log(data);
+        this.titulo = data.titulo;
+        document.title = `AdminPro - ${this.titulo}`;
+      });
+  }
 
   ngOnInit(): void {
   }
 
-  getDataRuta(){
-    this.router.events
-    .pipe(
-      filter((event: any) => event instanceof ActivationEnd),
-      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-      map((event: ActivationEnd) => event.snapshot.data)
+  ngOnDestroy(): void {
+    this.tituloSub$.unsubscribe();
+  }
+  getDataRuta() {
+    return this.router.events
+      .pipe(
+        filter((event: any) => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data)
+      );
 
-    )
-    .subscribe((data: any) => {
-      console.log(data);
-      this.titulo= data.titulo;
-      document.title = `AdminPro - ${this.titulo}`;
-    });
   }
 
 }
