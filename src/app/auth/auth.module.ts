43554655421f8f8743217d9f47ce4b8fc569e8d5
Carlyle Komalram <glaw14@gmail.com>
2022-09-10 10:35:29 -0500

    agregando reactive form -  registrar usuario

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [LoginComponent,RegisterComponent],
   imports: [
-    CommonModule, RouterModule, FormsModule
+    CommonModule, RouterModule, FormsModule, ReactiveFormsModule
   ],
   exports: [LoginComponent,RegisterComponent]
 })
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls:['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent   {
 
-  constructor() { }
+  public registerForm= this.formbuilder.group({
+    name: ['Carlyle Komalram', [Validators.required, Validators.minLength(3)]],
+    email: ['ckomalram@bandelta.com', [Validators.required]],
+    password: ['12345', [Validators.required, Validators.minLength(5)]],
+    password2: ['12345', [Validators.required, Validators.minLength(5)]],
+    terms: [false, [Validators.required]]
+  });
 
-  ngOnInit(): void {
+  constructor(private formbuilder: FormBuilder) { }
+
+  createUser(){
+    console.log(this.registerForm.value);
   }
+
+
 
 }
