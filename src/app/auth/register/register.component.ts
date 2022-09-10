38bb6468346033ab5c83ee4f1cb6d9f01ad03d8e
Carlyle Komalram <glaw14@gmail.com>
2022-09-10 10:52:15 -0500

    agregando validaciones - register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -4,24 +4,52 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls:['./register.component.css']
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent   {
+export class RegisterComponent {
 
-  public registerForm= this.formbuilder.group({
-    name: ['Carlyle Komalram', [Validators.required, Validators.minLength(3)]],
-    email: ['ckomalram@bandelta.com', [Validators.required]],
-    password: ['12345', [Validators.required, Validators.minLength(5)]],
-    password2: ['12345', [Validators.required, Validators.minLength(5)]],
+  public formSubmitted = false;
+
+  public registerForm = this.formbuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+    password2: ['', [Validators.required, Validators.minLength(5)]],
     terms: [false, [Validators.required]]
   });
 
   constructor(private formbuilder: FormBuilder) { }
 
-  createUser(){
-    console.log(this.registerForm.value);
+  createUser() {
+    this.formSubmitted = true;
+    // console.log(this.registerForm.value);
+    // console.log(this.registerForm);
+    // console.log(this.registerForm.valid);
+
+
+    if (this.registerForm.valid) {
+      console.log('Posteando formulario');
+    }else{
+      console.log('Formulario no es correcto..');
+    }
   }
 
+  campoNoValido(campo: string): boolean {
+
+    if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  aceptaTerminos() {
+
+    return !this.registerForm.get('terms')?.value && this.formSubmitted;
+
+
+
+  }
 
 
 }
