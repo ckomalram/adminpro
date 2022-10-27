e993565edb50f07a1f2b563588331c34964f9f9a
ckomalram <glaw14@gmail.com>
2022-10-26 23:29:07 -0500

    ux modal imagen

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,17 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { NgChartsModule } from 'ng2-charts';
 
 import { DonaComponent } from './dona/dona.component';
+import { ModalImagenComponent } from './modal-imagen/modal-imagen.component';
 
 
 
 @NgModule({
   declarations: [
     IncrementadorComponent,
-    DonaComponent
+    DonaComponent,
+    ModalImagenComponent
   ],
   imports: [
     CommonModule, FormsModule,NgChartsModule
   ],
-  exports: [IncrementadorComponent,DonaComponent]
+  exports: [IncrementadorComponent,DonaComponent,
+    ModalImagenComponent]
 })
 export class ComponentsModule { }
